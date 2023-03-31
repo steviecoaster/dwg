15a5ee72da9df1c61f1c61afaf73925db103551a
Stephen Valdinger <stephen@chocolatey.io>
2023-03-31 12:01:14 -0700

    Fix build script

--- build.ps1
+++ build.ps1
@@ -169,15 +169,19 @@ process {
         $GeneratePackage {}
 
         $CopyTemplate {
+
+            Write-host "Working from $PWD"
             $chocolateyTemplateFolder = Join-Path $env:ChocolateyInstall -ChildPath 'templates'
 
             if(-not (Test-Path $chocolateyTemplateFolder)){
                 $null = New-Item $chocolateyTemplateFolder
             }
 
-            $trueViewTemplate = Join-Path $PSScriptRoot -ChildPath 'templates\trueview'
+            $trueViewTemplate = Join-Path $PWD -ChildPath 'templates\trueview'
 
-            Write-Host $trueViewTemplate
+            Write-Host "Got template: $trueViewTemplate"
+            Write-Host "Destination: $chocolateyTemplateFolder"
+            
             Copy-Item $trueViewTemplate -Destination $chocolateyTemplateFolder -Force
         }
         
