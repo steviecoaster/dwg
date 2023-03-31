58d2583bfe716267601601cda834e47ca7be6ec0
Stephen Valdinger <stephen@chocolatey.io>
2023-03-31 13:00:39 -0700

    remove some shit

--- build.ps1
+++ build.ps1
@@ -177,11 +177,11 @@ process {
                 $null = New-Item $chocolateyTemplateFolder
             }
 
-            $trueViewTemplate = Join-Path $PWD -ChildPath 'templates\trueview'
+            $trueViewTemplate = Join-Path $PWD -ChildPath 'templates\trueview\'
 
             Write-Host "Got template: $trueViewTemplate"
             Write-Host "Destination: $chocolateyTemplateFolder"
-            
+
             Copy-Item $trueViewTemplate -Destination $chocolateyTemplateFolder -Force
         }
         
